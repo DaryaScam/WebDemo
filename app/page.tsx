20ee4c57a0155d71dc6497392db756a9b3664ed4
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2024-12-02 19:29:47 +1300

    Updated footer

--- app/page.tsx
+++ app/page.tsx
@@ -108,9 +108,11 @@ export default function Home() {
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <span>A DaryaScam Project. Developed by Yuriy Ackermann</span>
+
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/DaryaScam/Web-Demo"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -121,11 +123,11 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Code
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/DaryaScam"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -140,7 +142,7 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://DaryaScam.info"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -151,7 +153,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to DaryaScam.info →
         </a>
       </footer>
     </div>
